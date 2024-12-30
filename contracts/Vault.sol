d79e94c45ceba5b0da7a8db80a5ca3b9fc1af516
Eduardo <eduardo@MacBook-Pro-de-Eduardo.local>
2024-12-30 06:45:19 -0600

    vault updated

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -3,19 +3,18 @@ pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract TokenVault is Ownable { 
+contract TokenVault is Ownable, AccessControl { 
     IERC20 public token;
+    bytes32 public constant VAULTOWNER_ROLE = keccak256("VAULTOWNER_ROLE"); 
 
     constructor(IERC20 _token) Ownable(msg.sender) {
         token = _token;
+        _grantRole(VAULTOWNER_ROLE, msg.sender);
     }
 
-    function deposit(uint256 _amount) public {
-        require(token.transferFrom(msg.sender, address(this), _amount), "Transfer failed");
-    }
-
-    function withdraw(address _recipient, uint256 _amount) public onlyOwner { 
+    function transfer(address _recipient, uint256 _amount) public onlyRole(VAULTOWNER_ROLE) { 
         require(token.transfer(_recipient, _amount), "Transfer failed");
     }
 
