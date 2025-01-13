0f3d1e5ab3a3369dc86180f8f7a654e84b6aa292
Juan Pablo Donato <donatojp1996@gmail.com>
2025-01-13 11:38:57 -0300

    CucumberStudio integration + HipTest publisher + Scenarios UIDs

--- automation/cucumber-push-results.sh
+++ automation/cucumber-push-results.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+# Execution enviroment can be added based on current branch
+# docker run -it --rm -u $UID -v $(pwd):/app hiptest/hiptest-publisher --config-file=htpub-push-results.conf --execution-environment=local
 docker run -it --rm -u $UID -v $(pwd):/app hiptest/hiptest-publisher --config-file=htpub-push-results.conf
